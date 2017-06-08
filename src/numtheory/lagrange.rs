6aa66544d2dd4d17e3c3e1dd53840a95dd052065
Morten Dahl <mortendahlcs@gmail.com>
2017-06-08 20:13:54 +0200

    Lagrange improvements

--- src/numtheory/lagrange.rs
+++ src/numtheory/lagrange.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2016 rust-threshold-secret-sharing developers
+// Copyright (c) 2017 rust-threshold-secret-sharing developers
 //
 // Licensed under the Apache License, Version 2.0
 // <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
@@ -10,42 +10,66 @@
 
 use ::fields::Field;
 
-/// Performs a Lagrange interpolation in field Zp at the origin
+pub struct LagrangeConstants<F: Field>(Vec<F::E>);
+
+impl<F: Field> LagrangeConstants<F> 
+{
+    pub fn compute(point: &F::E, points: &[F::E], field: &F) -> LagrangeConstants<F> {
+        let mut constants = Vec::with_capacity(points.len());
+        for i in 0..points.len() {
+            let xi = &points[i];
+            let mut num = field.one();
+            let mut denum = field.one();
+            for j in 0..points.len() {
+                if j != i {
+                    let xj = &points[j];
+                    num = field.mul(num, field.sub(xj, point));
+                    denum = field.mul(denum, field.sub(xj, xi));
+                }
+            }
+            let coef = field.mul(num, field.inv(denum));
+            constants.push(coef);
+        }
+        
+        LagrangeConstants(constants)
+    }
+    
+    /// Note that care must be taken to provide the same `field` as the one used 
+    /// for computing the constants!
+    pub fn interpolate(&self, values: &[F::E], field: &F) -> F::E {
+        let ref constants = self.0;
+        assert_eq!(values.len(), constants.len());
+        // compute weighted sum
+        values.iter()
+            .zip(constants)
+            .map(|(v, c)| field.mul(v, c))
+            .fold(field.zero(), |a, b| field.add(a, b))
+    }
+}
+
+/// Performs Lagrange interpolation at the specified point,
 /// for a polynomial defined by `points` and `values`.
 ///
 /// `points` and `values` are expected to be two arrays of the same size, containing
 /// respectively the evaluation points (x) and the value of the polynomial at those point (p(x)).
+pub fn lagrange_interpolation_at_point<F>(point: &F::E, points: &[F::E], values: &[F::E], field: &F) -> F::E
+where F: Field, F::E: Clone
+{
+    assert_eq!(points.len(), values.len());
+    let constants = LagrangeConstants::compute(point, points, field);
+    constants.interpolate(values, field)
+}
+
+/// Performs Lagrange interpolation at the origin,
+/// for a polynomial defined by `points` and `values`.
+/// The result is the value of the polynomial at x=0.
 ///
-/// The result is the value of the polynomial at x=0. It is also its zero-degree coefficient.
-///
-/// This is obviously less general than `newton_interpolation_general` as we
-/// only get a single value, but it is much faster.
+/// `points` and `values` are expected to be two arrays of the same size, containing
+/// respectively the evaluation points (x) and the value of the polynomial at those point (p(x)).
 pub fn lagrange_interpolation_at_zero<F>(points: &[F::E], values: &[F::E], field: &F) -> F::E
 where F: Field, F::E: Clone
 {
-    // TODO coef computations could be reused
-    
-    assert_eq!(points.len(), values.len());
-    // Lagrange interpolation for point 0
-    let mut acc = field.zero();
-    for i in 0..values.len() {
-        // compute Lagrange coefficient
-        let xi = &points[i];
-        let mut num = field.one();
-        let mut denum = field.one();
-        for j in 0..values.len() {
-            if j != i {
-                let xj = &points[j];
-                num = field.mul(num, xj);
-                denum = field.mul(denum, field.sub(xj, xi));
-            }
-        }
-        let coef = field.mul(num, field.inv(denum));
-        // update sum
-        let yi = &values[i];
-        acc = field.add(acc, field.mul(yi, coef));
-    }
-    acc
+    lagrange_interpolation_at_point(&field.zero(), points, values, field)
 }
 
 #[cfg(test)]
@@ -53,6 +77,23 @@ mod tests {
     
     use super::*;
     use ::fields::*;
+    
+    fn test_interpolation_from_constants<F>()
+    where F: PrimeField + Encode<u32> + Decode<u32>, F::P: From<u32>, F::E: Clone
+    {
+        let ref field = F::new(17.into());
+
+        let poly = field.encode_slice([4, 3, 2, 1]);
+        let points = field.encode_slice([5, 6, 7, 8, 9]);
+        
+        let values = points.iter()
+            .map(|point| ::numtheory::mod_evaluate_polynomial(&poly, point.clone(), field)) // TODO no need to clone
+            .collect::<Vec<_>>();
+        
+        let constants = LagrangeConstants::compute(&field.zero(), &points, field);
+        let value = constants.interpolate(&values, field);
+        assert_eq!(field.decode(value), 4);
+    }
     
     fn test_lagrange_interpolation_at_zero<F>() 
     where F: PrimeField + Encode<u32> + Decode<u32>, F::P: From<u32>, F::E: Clone
@@ -73,6 +114,7 @@ mod tests {
     macro_rules! all_tests {
         ($field:ty) => {
             use super::*;
+            #[test] fn test_interpolation_from_constants() { super::test_interpolation_from_constants::<$field>(); }
             #[test] fn test_lagrange_interpolation_at_zero() { super::test_lagrange_interpolation_at_zero::<$field>(); }
         }
     }
